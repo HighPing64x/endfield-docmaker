e87dfc3b9ee33207c75ab282a5939f2fe392212f
Naptie <naptie@qq.com>
2026-04-15 20:40:34 +0800

    feat: add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,12 @@
   import { ModeWatcher } from 'mode-watcher';
   import Footer from '$lib/components/Footer.svelte';
   import { m } from '$lib/paraglide/messages';
+  import { dev } from '$app/environment';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
   let { children } = $props();
+
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
